bb7e33b12b1f15befe1668a0a0a5bf27c391446d
Rafael Mutis-Maschmann <105695147+rmutis@users.noreply.github.com>
2024-06-21 08:52:46 +0200

    Test on functionalities

--- dataframe.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-d = {'habit name': ["Sport", "Sport", "Sleep"], 'Status': ["closed", "open", "open"], 'Due': ["11-01-2024", "01-02-2024", "04-03-2024"]}
-df = pd.DataFrame(data=d)
-blub = df[(df["habit name"] == "Sport") & (df["Status"] == "open")]
-print(blub.iloc[0,2])
